2be3f0d74088140cc894e3e688348f4236a3201f
mahapatroniru <mahapatroniranjankumar@gmail.com>
2026-09-05 13:26:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api id here"
-api_hash = "enter your api hash here"
-bot_token = "enter your bot token here"
-auth_users = [enter your auth user id here]
+api_id = "952608"
+api_hash = "8d8d0ad8e3d4bcd54420190f57da78ad"
+bot_token = "6355112108:AAGwzt9JQTOoK8ZGtc2gTFKGpiCIr6RFR0U"
+auth_users = [818269274]
